bf426a37d10f50940434d50031eb570ccbefddbe
strzupak <strzupak@gmail.com>
2014-09-19 18:22:34 +0200

    adding whisky.py update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from piwa import Piwo
+from whisky import Whisky
 
 def helloWorld(name):
 	return "Hello, %s" % (name)
@@ -8,3 +9,5 @@ if __name__ == "__main__":
 	print helloWorld("Denat")
 	tyskie = Piwo("Tyskie")
 	print tyskie.pokazPiwo()
+	grants = Whisky("Grants")
+	print grants.pokazWhisky()
---
+++ whisky.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+class Whisky(object):
+	def __init__(self, name):
+		self.name = name
+		
+	def pokazWhisky(self):
+		return self.name
