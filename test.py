25061340e19d39c133d1b0dd54a96e133660e5f9
strzupak <strzupak@gmail.com>
2014-09-19 18:04:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+from piwo import Piwo
+
 def helloWorld(name):
 	return "Hello, %s" % (name)
 	
 if __name__ == "__main__":
 	print helloWorld("Denat")
+	tyskie = Piwo("Tyskie")
+	print tyskie.pokazPiwo()
